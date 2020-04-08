4d03b3ef07490dfcd7a3e7e4e1999e89441c7c72
cwelim <chandra.welim@gmail.com>
2020-04-08 21:05:16 +0700

    Init features

---
+++ Profile/Profile/Profile.h
@@ -0,0 +1,19 @@
+//
+//  Profile.h
+//  Profile
+//
+//  Created by Chandra Welim on 08/04/20.
+//  Copyright © 2020 Chandra Welim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Profile.
+FOUNDATION_EXPORT double ProfileVersionNumber;
+
+//! Project version string for Profile.
+FOUNDATION_EXPORT const unsigned char ProfileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Profile/PublicHeader.h>
+
+
---
+++ Ticket/Ticket/Ticket.h
@@ -0,0 +1,19 @@
+//
+//  Ticket.h
+//  Ticket
+//
+//  Created by Chandra Welim on 08/04/20.
+//  Copyright © 2020 Chandra Welim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Ticket.
+FOUNDATION_EXPORT double TicketVersionNumber;
+
+//! Project version string for Ticket.
+FOUNDATION_EXPORT const unsigned char TicketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ticket/PublicHeader.h>
+
+

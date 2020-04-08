e482424f1101524a5c3c08423b9ce56c2184b9af
cwelim <chandra.welim@gmail.com>
2020-04-08 19:56:51 +0700

    Init

---
+++ VW-project/VW-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VW-project
+//
+//  Created by Chandra Welim on 08/04/20.
+//  Copyright © 2020 Chandra Welim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

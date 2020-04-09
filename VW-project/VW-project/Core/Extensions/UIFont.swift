dc151a75c1bc43a27667d11a14fcbb02ba0e34b8
Azis Senoaji Prasetyotomo <>
2020-04-09 15:07:07 +0700

    New tabbar and fonts

---
+++ VW-project/VW-project/Core/Extensions/UIFont.swift
@@ -0,0 +1,17 @@
+//
+//  UIFont.swift
+//  VW-project
+//
+//  Created by venazp on 09/04/20.
+//  Copyright © 2020 Chandra Welim. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static func monseratBold(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "Montserrat-Bold", size: size)!
+    }
+    
+}
--- VW-project/VW-project/VWTabBarController.swift
+++ VW-project/VW-project/VWTabBarController.swift
@@ -12,35 +12,57 @@ class VWTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTabbar()
+        
+        viewControllers = [home(), map(), itenerary(), myTickets()]
+
+    }
+    
+    private func setTabbar() {
+        tabBar.barTintColor = .white
+        tabBar.layer.masksToBounds = true
+        tabBar.isTranslucent = true
+        tabBar.barStyle = .black
+        tabBar.layer.cornerRadius = 16
+        tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        tabBar.barTintColor = .black
+        UITabBarItem.appearance().setTitleTextAttributes(
+            [NSAttributedString.Key.font: UIFont.monseratBold(10)], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes(
+            [NSAttributedString.Key.font: UIFont.monseratBold(10)], for: .normal)
 
     }
     
     func home() -> UINavigationController {
         let homeVC = HomeBuilder().main()
         homeVC.title = "Home"
-        homeVC.tabBarItem = UITabBarItem(title: "Ticket", image: nil, tag: 0)
-        return UINavigationController(rootViewController: ticketVC)
+        homeVC.tabBarItem = UITabBarItem(title: "HOME", image: #imageLiteral(resourceName: "home_ico-inactive"), tag: 0)
+        homeVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "home_ico-active")
+        return UINavigationController(rootViewController: homeVC)
     }
     
     func map() -> UINavigationController {
         let mapVC = MapBuilder().main()
         mapVC.title = "Map"
-        mapVC.tabBarItem = UITabBarItem(title: "Map", image: nil, tag: 0)
+        mapVC.tabBarItem = UITabBarItem(title: "MAP", image: #imageLiteral(resourceName: "map_ico-inactive"), tag: 0)
+        mapVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "map_ico-active")
         return UINavigationController(rootViewController: mapVC)
     }
     
     func itenerary() -> UINavigationController {
-        let profileVC = ProfileBuilder().main()
-        profileVC.title = "Itenerary"
-        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: nil, tag: 0)
-        return UINavigationController(rootViewController: profileVC)
+        let iteneraryVC = IteneraryBuilder().main()
+        iteneraryVC.title = "Itenerary"
+        iteneraryVC.tabBarItem = UITabBarItem(title: "ITENERARY", image: #imageLiteral(resourceName: "itenerary_ico-inactive"), tag: 0)
+        iteneraryVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "itenerary_ico-active")
+        return UINavigationController(rootViewController: iteneraryVC)
     }
     
     func myTickets() -> UINavigationController {
-        let profileVC = ProfileBuilder().main()
-        profileVC.title = "My Tickets"
-        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: nil, tag: 0)
-        return UINavigationController(rootViewController: UIViewController())
+        let myTicketVC = MyTicketsBuilder().main()
+        myTicketVC.title = "My Tickets"
+        myTicketVC.tabBarItem = UITabBarItem(title: "MY TICKETS", image: #imageLiteral(resourceName: "ticket_ico-inactive"), tag: 0)
+        myTicketVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "ticket_ico-active")
+        return UINavigationController(rootViewController: myTicketVC)
     }
 
 }
